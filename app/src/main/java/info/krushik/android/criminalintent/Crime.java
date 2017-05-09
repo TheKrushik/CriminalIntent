dc52e29027dd50745f6699c6975f7f871a0485af
KrusH <krushik@ukr.net>
2017-05-10 00:42:31 +0300

    Initial commit

---
+++ app/src/main/java/info/krushik/android/criminalintent/Crime.java
@@ -0,0 +1,30 @@
+package info.krushik.android.criminalintent;
+
+
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    public Crime() {
+// Генерирование уникального идентификатора
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public void setmId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
